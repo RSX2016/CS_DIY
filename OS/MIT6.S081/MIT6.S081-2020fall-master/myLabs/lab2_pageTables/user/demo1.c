9b368aa0548b335b9cfeca419b089d40aae16976
shixin.ruan <1005637196@qq.com>
2023-05-21 22:02:12 +0800

    sync

--- OS/MIT6.S081/MIT6.S081-2020fall-master/myLabs/lab2_pageTables/user/demo1.c
+++ OS/MIT6.S081/MIT6.S081-2020fall-master/myLabs/lab2_pageTables/user/demo1.c
@@ -1,14 +1,23 @@
-int sum_to(int n) {
-    int acc = 0;
-    for (int i = 0; i <= n; i++) {
-        acc += i;
+void swap(long long int v[], int k)
+{
+    long long int temp;
+    temp = v[k];
+    v[k] = v[k+1];
+    v[k+1] = temp;
+}
+
+void sort(long long int v[], int n)
+{
+    int i, j;
+    for (i = 0; i < n; i +=1) {
+        for (j = i - 1; j >= 0 && v[j] > v[j+1]; j -= 1) {
+            swap(v, j);
+        } 
     }
-    return acc;
 }
 
 int
 main(int argc, char *argv[])
 {
-    int n = sum_to(10);
-    return n;
+    return 0;
 }
